128df8d02afc41fbfbe095629593bcf94cff1171
gouthamk16 <gouthamrakesh@icloud.com>
2025-01-20 20:42:29 +0530

    change

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="attogradDB",  # Name of your package
-    version="0.3.1",  # Initial version
+    version="0.4",  
     description="A simple vector database for fast similarity search",  # Short description
     long_description=open("README.md").read(),  # Use README.md for long description
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/vector-db",  # Your project URL (GitHub, etc.)
+    url="https://github.com/gouthamk16/attogradDB",  # Your project URL (GitHub, etc.)
     author="Goutham Krishnan",
     license="MIT",  # License type
     packages=find_packages(),  # Automatically find packages in the folder
